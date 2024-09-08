dad3779c26fb83c6f9c2d79a9f4a30ba70a14f96
Nikolaj J-K <kagenogmig@gmail.com>
2024-09-08 14:39:14 +0200

    nikolaj

--- EndController.cs
+++ EndController.cs
@@ -4,6 +4,9 @@ using System;
 public partial class EndController : Node
 {
 	// Called when the node enters the scene tree for the first time.
+
+	[Export]
+	private Node3D sprite3D;
 	public override void _Ready()
 	{
 	}
@@ -11,5 +14,9 @@ public partial class EndController : Node
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void _on_timer_timeout() {
+		sprite3D.Visible = true;
 	}
 }
